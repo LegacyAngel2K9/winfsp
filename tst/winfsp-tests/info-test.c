3876efde591c2bd6f3594db912da510821de7777
Angel Knutsen Aune <djangel@live.no>
2026-08-01 22:36:21 +0200

    Fix #494 support FileIdInformation

--- tst/winfsp-tests/info-test.c
+++ tst/winfsp-tests/info-test.c
@@ -297,6 +297,30 @@ void getfileinfo_dotest(ULONG Flags, PWSTR Prefix, ULONG FileInfoTimeout)
     ASSERT(0 == FileInfo.nFileSizeLow && 0 == FileInfo.nFileSizeHigh);
     ASSERT(1 == FileInfo.nNumberOfLinks);
 
+    if (-1 != Flags)
+    {
+        struct
+        {
+            /* FILE_ID_INFO is missing from the old SDK versions this project still supports. */
+            ULONGLONG VolumeSerialNumber;
+            UINT8 FileId[16];
+        } IdInfo;
+        union
+        {
+            UINT64 IndexNumber;
+            UINT8 FileId[16];
+        } ExpectedFileId;
+
+        memset(&ExpectedFileId, 0, sizeof ExpectedFileId);
+        Success = GetFileInformationByHandleEx(Handle, 0x12/*FileIdInfo*/, &IdInfo, sizeof IdInfo);
+        if (Success)
+        {
+            ExpectedFileId.IndexNumber =
+                ((UINT64)FileInfo.nFileIndexHigh << 32) | (UINT64)FileInfo.nFileIndexLow;
+            ASSERT(0 == memcmp(ExpectedFileId.FileId, IdInfo.FileId, sizeof IdInfo.FileId));
+        }
+    }
+
     CloseHandle(Handle);
 
     memfs_stop(memfs);
